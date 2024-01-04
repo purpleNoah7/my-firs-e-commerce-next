ddf5db425fce635ca9a46ed733b442ea1e42cb8d
purpleNoah7 <lodasr6@gmail.com>
2024-01-03 23:54:47 -0500

    Codigo arreglado para deploy

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
+            <a href="#" className="hover:underline">
               Leandro
             </a>
             . Powered Fake Store API
@@ -13,22 +13,17 @@ export default function Footer() {
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
-                About
+                Github
               </a>
             </li>
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
+                Instagram
               </a>
             </li>
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
+                Linkedin
               </a>
             </li>
           </ul>
